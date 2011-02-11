9683070f3b734133d090ae9831f12f12af0f9bb1
Bart vd Ende <bartsidee@gmail.com>
2011-02-11 15:01:38 +0100

    tmf update

--- apps/tv/dev/tmf/tmf.py
+++ apps/tv/dev/tmf/tmf.py
@@ -25,101 +25,37 @@ class Module(object):
 
         streamlist = list()
         for info in soup.findAll('li'):
-            title = re.compile('--(.*?)$', re.DOTALL + re.IGNORECASE).search(str(info.span.contents[0])).group(1)
-            id = re.compile('--(.*?)--', re.DOTALL + re.IGNORECASE).search(str(info.span.contents[0])).group(1)
+            title = re.compile('--(.*?)$', re.DOTALL + re.IGNORECASE).search(str(info.span.contents[0])[4:]).group(1)
+            id = re.compile('---(.*?)---', re.DOTALL + re.IGNORECASE).search(str(info.span.contents[0])).group(1)
             stream = ba.CreateStream()
             stream.SetName(title)
             stream.SetId(id)
             streamlist.append(stream)
-
         return streamlist
-    
-    def Episode(self, stream_name, stream_id, page, totalpage):
-        data = ba.FetchUrl(stream_id, 3600)
-
-        if data == "":
-            mc.ShowDialogNotification("No episode found for " + str(stream_name))
-            return ba.CreateEpisode()
-
-        soup = BeautifulSoup(data, convertEntities="xml", smartQuotesTo="xml")
-        totalpage = len(soup.findAll('tr', 'srh'))
-
-        try:
-            episode_url = re.compile('VideoExpander.subheadingClicked\((.*?)\)"', re.DOTALL + re.IGNORECASE).search(str(data)).group(1)
-        except:
-            mc.ShowDialogNotification("No episode found for " + str(stream_name))
-            return ba.CreateEpisode()
-
-        season_number = re.compile('season_number=(.*?)\&', re.DOTALL + re.IGNORECASE).search(str(episode_url)).group(1)
-        show_id = re.compile('show_id=(.*?)\&', re.DOTALL + re.IGNORECASE).search(str(episode_url)).group(1)
-
-        pp = []
-        for i in range(0,totalpage):
-            pp.append(str(int(season_number) - i))
-        intpage = int(page) - 1
-
-        url = "http://www.hulu.com/videos/season_expander?order=desc&page=1&season_number=" + str(pp[intpage]) + "&show_id=" + str(show_id) + "&sort=season&video_type=episode"
-
-        data = ba.FetchUrl(url)
-        data = re.compile('srh-bottom-' + pp[intpage] +'", "(.*?)"\);', re.DOTALL + re.IGNORECASE).search(str(data)).group(1)
-        data = data.replace('\\u003c','<').replace('\\u003e','>').replace('\\','')
-        soup = BeautifulSoup(data)
-
-        episodelist = list()
-        name = []
-        link = []
-        number = []
-        thumb = []
-        for tmp in soup.findAll('td', {'class':'c0'}):
-            number.append(tmp.contents[0])
-
-        i = 0
-        b = 0
-        for tmp in soup.findAll('td', {'class':'c1'}):
-            name.append(tmp.a.contents[0])
-            link.append(tmp.a['href'])
-            try:
-                thumb.append(self.GetThumb(re.compile('/watch/(.*?)/', re.DOTALL + re.IGNORECASE).search(str(tmp.a['href'])).group(1)))
-            except:
-                thumb.append('')
-            b += 1
-            if len(tmp.findAll('div', 'vex-h')) == 0:
-                i += 1
-
-        if i != b: totalpage = page
-
-        for x in range(0,i):
-            episode = ba.CreateEpisode()
-            episode.SetName(stream_name)
-            episode.SetId(link[x])
-            episode.SetDescription('Episode: ' + number[x] + ' - '  + name[x])
-            episode.SetThumbnails(thumb[x])
-            episode.SetDate('Season: ' + pp[intpage])
-            episode.SetPage(page)
-            episode.SetTotalpage(totalpage)
-            episodelist.append(episode)
-
-        return episodelist
 
     def Play(self, stream_name, stream_id, subtitle):
-        path = self.tinyurl(stream_id)
+        url = 'http://www.tmf.nl/xml/videoplayer/mediaGen.php?id='+stream_id
+        data = ba.FetchUrl(url)
+        soup = BeautifulSoup(data, convertEntities="xml", smartQuotesTo="xml")
+
+        bitrate = []
+        for info in soup.findAll('rendition', {'bitrate':True}):
+            bitrate.append(int(info['bitrate']))
+        bitrate.sort()
+        max = str(bitrate[-1])
+
+        rendition = soup.find('rendition', {'bitrate':max})
+        path = rendition.src.contents[0]
+        print path
+        rtmplist = path.split('ondemand/')
+        print rtmplist
+
         play = ba.CreatePlay()
-        play.SetPath(quote_plus(path))
-        play.SetDomain('bartsidee.nl')
-        play.SetJSactions(quote_plus('http://bartsidee.nl/boxee/apps/hulu.js'))
+        playPath =  rtmplist[1]
+        rtmpURL = rtmplist[0] + 'ondemand'
+        authPath = ''
+
+        play.SetRTMPPath(playPath)
+        play.SetRTMPDomain(rtmpURL)
+        play.SetRTMPAuth(authPath)
         return play
-
-    def tinyurl(self, params):
-        url = "http://tinyurl.com/api-create.php?url=" + str(params)
-        return ba.FetchUrl(url)
-
-    def GetThumb(self, id):
-        url = "http://www.hulu.com/videos/info/" + str(id)
-        data = ba.FetchUrl(url,0,True)
-        try:
-            return re.compile('"thumbnail_url":"(.*?)"', re.DOTALL + re.IGNORECASE).search(str(data)).group(1)
-        except:
-            try:
-                return re.compile('"thumbnail_url": "(.*?)"', re.DOTALL + re.IGNORECASE).search(str(data)).group(1)
-            except:
-                return str('')
